22702173019687635ae82c2bc50420d3fca6552d
lafifii <fifii.geral@gmail.com>
2022-07-01 03:30:55 -0500

    consensus

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ from hashlib import sha256
 import hashlib
 import json
 import time
-import copy
 from urllib import response
 from uuid import uuid4
+from urllib.parse import urlparse
+import requests
 
 from flask import Flask, jsonify, request, send_from_directory
-from numpy import block
-import requests
-from urllib3 import proxy_from_url
 
 app = Flask(__name__)
 PoW = 4
@@ -18,9 +16,54 @@ class Blockchain:
     def __init__(self):
         self.chain = []
         self.current = []
+        self.nodes = set()
 
         self.newBlock(previous_hash=0, proof=0)
     
+    def registerNode(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def isValid(self, chain):
+        lblock = chain[0]
+        curr = 1
+
+        while curr < len(chain):
+            block = chain[curr]
+
+            if block['previous_hash'] != self.hash(lblock):
+                return False 
+            
+            if not self.valid_proof(lblock['proof'], block['proof']):
+                return False
+            
+            lblock = block 
+            curr+= 1
+        
+        return True
+    
+    def solve(self):
+        nexts = self.nodes
+        nchain = None
+
+        mx = len(self.chain)
+
+        for node in nexts:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                ln = response.json()['length']
+                chain = response.json()['chain']
+
+                if ln > mx and self.isValid(chain):
+                    mx = ln
+                    nchain = chain
+        
+        if nchain:
+            self.chain = nchain
+            return True
+
+
     def newBlock(self, proof, previous_hash=None):
         block = {
             'index': len(self.chain) + 1,
@@ -113,6 +156,41 @@ def full_chain():
         'chain': blockchain.chain,
         'tamaño': len(blockchain.chain),
     }
+
+    return jsonify(response), 200
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error en lista de nodos", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'Se agregaron los nuevos nodos',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    ok = blockchain.resolve_conflicts()
+
+    if not ok:
+        response = {
+            'message': 'La cadena fue remplazada',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'La cadena es autoritativa',
+            'chain': blockchain.chain
+        }
 
     return jsonify(response), 200
 
